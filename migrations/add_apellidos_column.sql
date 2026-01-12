25031e71da717f4cff29b753317f877b156e2d6b
AlvaroPavon <alvaropavonmartinez7@gmail.com>
2026-01-12 16:47:11 +0000

    auto-commit for e60e1b1a-e4a2-4773-9221-1e8783641b51

--- migrations/add_apellidos_column.sql
+++
@@ -1,9 +0,0 @@
--- Migración: Añadir columna 'apellidos' a la tabla usuarios
--- Ejecutar este script si la tabla ya existe sin la columna apellidos
-
-ALTER TABLE usuarios 
-ADD COLUMN apellidos VARCHAR(100) NOT NULL DEFAULT '' 
-AFTER nombre;
-
--- Confirmar que la columna fue añadida
--- SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'usuarios';
