5dbf1c50822c46c5db3a4e64643d0d2ccb729eef
AlvaroPM <alvaropavonmartinez7@gmail.com>
2026-01-14 13:52:06 +0100

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'alvaropavon/stat-tracker',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c3cc57611930f792735150c3862559ca342f76a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'alvaropavon/stat-tracker' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c3cc57611930f792735150c3862559ca342f76a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
