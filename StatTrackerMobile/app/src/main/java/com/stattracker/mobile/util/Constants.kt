9b054db4431a8daacbd5ee48d8bc1c55cdc71088
AlvaroPM <alvaropavonmartinez7@gmail.com>
2026-03-17 00:34:01 +0100

    added

--- StatTrackerMobile/app/src/main/java/com/stattracker/mobile/util/Constants.kt
+++ StatTrackerMobile/app/src/main/java/com/stattracker/mobile/util/Constants.kt
@@ -4,10 +4,17 @@ package com.stattracker.mobile.util
  * Constantes de configuración de la aplicación
  */
 object Constants {
-    // API Base URL para XAMPP
-    // La ruta debe llegar hasta la carpeta 'public' del proyecto
-    const val BASE_URL = "http://10.0.2.2:8080/proyecto_imc/"
+    // --- CONFIGURACIÓN PARA MÓVIL REAL (HOTSPOT) ---
     
+    // IP del PC (Puerta de enlace del Hotspot)
+    private const val MY_IP = "192.168.137.1"
+    
+    // Basado en los logs exitosos anteriores, la raíz es 'proyecto_imc'
+    // El prefijo '/api/' ya lo añaden las funciones de la interfaz StatTrackerApi
+    const val BASE_URL = "http://$MY_IP:8080/proyecto_imc/"
+    
+    // ----------------------------------------------
+
     // Preferences
     const val PREF_NAME = "stattracker_prefs"
     const val KEY_TOKEN = "jwt_token"
